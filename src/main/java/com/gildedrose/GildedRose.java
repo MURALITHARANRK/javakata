df400371950f765d813d3b5653c56d73388f71a9
Muralitharan R K <muralitharan.rk@gmail.com>
2024-05-18 21:54:42 +0530

    JiraID-1234: Unit Test: Test case has been added for Conjured product and Implementation also added for the same

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,8 +11,9 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                 && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    items[i].quality = items[i].quality - 1;
+                    if (items[i].quality > 0 && items[i].name.equals("Conjured")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -40,7 +41,7 @@ class GildedRose {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
+            if (items[i].sellIn < 0 && !items[i].name.equals("Conjured")) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
