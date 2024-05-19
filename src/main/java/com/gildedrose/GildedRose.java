fd952039aa78227bc8fc8735ca94c0ab20c59645
Muralitharan R K <muralitharan.rk@gmail.com>
2024-05-19 22:31:28 +0530

    JiraID-1234: Refactor: Product Registry has been created to initialize the product service

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,12 +1,8 @@
 package com.gildedrose;
 
-import com.gildedrose.service.impl.*;
+import com.gildedrose.service.ProductRegistry;
 
 class GildedRose {
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String CONJURED = "Conjured";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -15,54 +11,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.name) {
-                case SULFURAS:
-                    sulfuras(item);
-                    break;
-                case AGED_BRIE:
-                    agedBrie(item);
-                    break;
-                case BACKSTAGE_PASSES:
-                    backstagePasses(item);
-                    break;
-                case CONJURED:
-                    conjured(item);
-                    break;
-                default:
-                    standardItems(item);
-                    break;
-            }
+            ProductRegistry.updateProductQuality(item);
         }
     }
-
-    private void standardItems(Item item) {
-        StandardItems standardItems = new StandardItems();
-        standardItems.updateProductQuality(item);
-        standardItems.updateProductSellIn(item);
-    }
-
-    private void agedBrie(Item item) {
-        AgedBrie agedBrie = new AgedBrie();
-        agedBrie.updateProductQuality(item);
-        agedBrie.updateProductSellIn(item);
-    }
-
-    private void backstagePasses(Item item) {
-        BackstagePasses backstagePasses = new BackstagePasses();
-        backstagePasses.updateProductQuality(item);
-        backstagePasses.updateProductSellIn(item);
-    }
-
-    private void conjured(Item item) {
-        Conjured conjured = new Conjured();
-        conjured.updateProductQuality(item);
-        conjured.updateProductSellIn(item);
-    }
-
-    private void sulfuras(Item item) {
-        Sulfuras sulfuras = new Sulfuras();
-        sulfuras.updateProductQuality(item);
-        sulfuras.updateProductSellIn(item);
-    }
-
 }
