3ea7490b8f5d875e72ca74ec5942c90bcfc89a31
Muralitharan R K <muralitharan.rk@gmail.com>
2024-05-19 22:42:36 +0530

    JiraID-1234: Refactor: Created constants MIN and MAX values for product quality

--- src/main/java/com/gildedrose/service/ProductUpdate.java
+++ src/main/java/com/gildedrose/service/ProductUpdate.java
@@ -10,14 +10,17 @@ import com.gildedrose.Item;
  */
 public class ProductUpdate {
 
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+
     /**
-     *  The Quality of the product should not be more than <b>50</b> as well as should not be in negative.
+     * The Quality of the product should not be more than <b>50</b> as well as should not be in negative.
      */
     public void updateQuality(Item item, int itemQuality) {
-        if(itemQuality>0){
-            item.quality = (Math.max(item.quality, (item.quality + itemQuality)) > 50) ? 50 : (item.quality + itemQuality);
-        }else{
-            item.quality = (Math.min(item.quality, (item.quality + itemQuality)) < 0) ? 0 : (item.quality + itemQuality);
+        if (itemQuality > 0) {
+            item.quality = (Math.max(item.quality, (item.quality + itemQuality)) > MAX_QUALITY) ? MAX_QUALITY : (item.quality + itemQuality);
+        } else {
+            item.quality = (Math.min(item.quality, (item.quality + itemQuality)) < MIN_QUALITY) ? MIN_QUALITY : (item.quality + itemQuality);
         }
     }
 
