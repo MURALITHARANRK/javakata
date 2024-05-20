95307288d414b5331dc889c634c84737fd222094
Muralitharan R K <muralitharan.rk@gmail.com>
2024-05-20 12:22:09 +0530

    JiraID-1234: Refactor: Implemented the default methods in StandardItems

--- src/main/java/com/gildedrose/service/impl/StandardItems.java
+++ src/main/java/com/gildedrose/service/impl/StandardItems.java
@@ -2,7 +2,6 @@ package com.gildedrose.service.impl;
 
 import com.gildedrose.Item;
 import com.gildedrose.service.ProductService;
-import com.gildedrose.service.ProductUpdate;
 
 /**
  * @author Muralitharan R K
@@ -12,20 +11,18 @@ import com.gildedrose.service.ProductUpdate;
  */
 public class StandardItems implements ProductService {
 
-    private final ProductUpdate productUpdate = new ProductUpdate();
-
     /**
      * @param item
      */
     @Override
     public void updateProductQuality(Item item) {
         int qualityValue;
-        if(productUpdate.isValidSellIn(item)){
+        if(isValidSellIn(item)){
             qualityValue = -2;
         }else{
             qualityValue = -1;
         }
-        productUpdate.updateQuality(item, qualityValue);
+        updateQuality(item, qualityValue);
     }
 
     /**
@@ -33,6 +30,6 @@ public class StandardItems implements ProductService {
      */
     @Override
     public void updateProductSellIn(Item item) {
-        productUpdate.updateSellIn(item);
+        updateSellIn(item);
     }
 }
