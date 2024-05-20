f3848dc9dc5e506c08aa46803076935d7bad9c2d
Muralitharan R K <muralitharan.rk@gmail.com>
2024-05-20 11:18:53 +0530

    JiraID-1234: Refactor: Default methods added in interface. The same updated in AgedBrie product.

--- src/main/java/com/gildedrose/service/ProductService.java
+++ src/main/java/com/gildedrose/service/ProductService.java
@@ -11,11 +11,47 @@ import com.gildedrose.Item;
  * <li>updateProductQuality method</li>
  * <li>updateProductSellIn method</li>
  * </ul>
+ * <p>Some common functionalities of the Product Service methods added under default method</p>
  */
 public interface ProductService {
+
+    int MAX_QUALITY = 50;
+    int MIN_QUALITY = 0;
 
     void updateProductQuality(Item item);
 
     void updateProductSellIn(Item item);
+
+    /**
+     * Validating the product whether it is valid to sell or not.
+     */
+    default boolean isValidSellIn(Item item) {
+        return item.sellIn <= 0;
+    }
+
+    /**
+     * The Quality of the product should not be more than <b>50</b> as well as should not be in negative.
+     */
+    default void updateQuality(Item item, int itemQuality) {
+        if (itemQuality > 0) {
+            item.quality = (Math.max(item.quality, (item.quality + itemQuality)) > MAX_QUALITY) ? MAX_QUALITY : (item.quality + itemQuality);
+        } else {
+            item.quality = (Math.min(item.quality, (item.quality + itemQuality)) < MIN_QUALITY) ? MIN_QUALITY : (item.quality + itemQuality);
+        }
+    }
+
+    /**
+     * Reduce the product sellIn value
+     */
+    default void updateSellIn(Item item) {
+        item.sellIn--;
+    }
+
+    /**
+     * Reset the product quality value
+     */
+    default void resetQuality(Item item) {
+        item.quality = 0;
+    }
 
 }
