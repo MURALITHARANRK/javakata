6e50b358d453effd6591295c12ec4812e4baa798
Muralitharan R K <muralitharan.rk@gmail.com>
2024-05-23 21:09:45 +0530

    JiraID-1234: Refactor: ProductList updated as class instead of enum

--- src/main/java/com/gildedrose/service/ProductList.java
+++ src/main/java/com/gildedrose/service/ProductList.java
@@ -4,13 +4,13 @@ package com.gildedrose.service;
  * @author Muralitharan R K
  * @project javakata
  * <p></p>
- * <p>All the Product names has been added in the enum ProductList.</p>
+ * <p>All the Product names has been added in the class ProductList.</p>
  *
  */
-public enum ProductList {
-    INSTANCE;
+public class ProductList {
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final String CONJURED = "Conjured";
+
 }
